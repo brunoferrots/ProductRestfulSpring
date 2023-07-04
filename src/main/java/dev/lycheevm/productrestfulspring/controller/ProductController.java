a10310d662439b1c06f6dfd87d09f71405f55edb
brunoferrots <brunoferrots@outlook.com>
2023-07-04 17:14:29 -0300

    Insert Gets and Put methods in the API

--- src/main/java/dev/lycheevm/productrestfulspring/controller/ProductController.java
+++ src/main/java/dev/lycheevm/productrestfulspring/controller/ProductController.java
@@ -5,13 +5,17 @@ import dev.lycheevm.productrestfulspring.model.Product;
 import dev.lycheevm.productrestfulspring.repository.ProductRepository;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import org.apache.catalina.connector.Response;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/products")
@@ -27,5 +31,30 @@ public class ProductController {
     public ResponseEntity<Product> createProduct(@RequestBody @Valid ProductDto productDto) {
         var product = new Product(productDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(product));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        return ResponseEntity.status(HttpStatus.OK).body(repository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getProduct(@PathVariable(value = "id") UUID id) {
+        Optional<Product> product = repository.findById(id);
+        if(product.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(product);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateProduct(@PathVariable(value = "id") UUID id, @RequestBody @Valid ProductDto productDto) {
+        var productSearched = repository.findById(id);
+        if (productSearched.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+        }
+        var product = productSearched.get();
+        BeanUtils.copyProperties(productDto, product);
+        return ResponseEntity.status(HttpStatus.OK).body(repository.save(product));
     }
 }
